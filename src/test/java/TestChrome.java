07fc8002b071e1cc84808da71b1cfba6de43b774
Ajnien <alice999061@gmail.com>
2023-01-18 00:44:02 +0200

    [master]: Project initial upload

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -1,12 +1,30 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TestChrome {
+    ChromeDriver driver = new ChromeDriver();
+    public static String GOOGLE_URL = "https://www.google.lv/?hl=en";
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+
+    }
     @Test
     public void chromeTest() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.google.lv/?hl=en");
-driver.findElement(By.xpath("//button//div[contains(text), 'Accept all'))]"));
+
+        driver.get(GOOGLE_URL);
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+        acceptButton.click();
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Qatar 2022");
+        searchField.sendKeys(Keys.ENTER);
+
+        Assert.assertEquals(driver.getTitle(),"Qatar 2022 - Google Search");
     }
 }
